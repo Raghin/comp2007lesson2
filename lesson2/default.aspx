e0bf65a6edd0c4670581a8a8b8685e768af59b16
Raghin <bratty.chum@gmail.com>
2015-05-11 14:17:08 -0400

    first commit

---
+++ lesson2/default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="lesson2._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lesson 2 Home page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblFirstname" runat="server"></asp:Label>
+        <asp:Literal ID="ltlFirstname" runat="server"></asp:Literal>
+        <asp:Textbox ID="txtFirstname" runat="server"></asp:Textbox>
+        <asp:Button ID="btnFirstname" runat="server" Text="ClickMe" OnClick="btnFirstname_Click" />
+        <asp:Label ID="lblX" runat="server" Visible="false"></asp:Label>
+
+    </div>
+    <div>
+        <asp:Literal ID="ltlToppings" runat="server" />
+        <asp:DropDownList runat="server" ID="ddlCountry">
+            <asp:ListItem Value="1" Text="Canada"></asp:ListItem>
+            <asp:ListItem Value="2" Text="Brazil"></asp:ListItem>
+            <asp:ListItem Value="3" Text="India"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:Button runat="server" ID="btnSelections" Text="Show Selections" OnClick="btnSelections_Click" />
+        <asp:CheckBoxList runat="server" ID="cblToppings">
+            <asp:ListItem Value="1" Text="Feta"></asp:ListItem>
+            <asp:ListItem Value="2" Text="Mushrooms"></asp:ListItem>
+            <asp:ListItem Value="3" Text="Tomato"></asp:ListItem>
+        </asp:CheckBoxList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ lesson2/default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lesson2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public int x;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //add code here
+            if (!IsPostBack)
+            {
+                x = 0;
+                x++;
+                lblX.Text = x.ToString();
+            }
+            //only increment x if page isnt being reloaded
+            
+        }
+
+        protected void btnFirstname_Click(object sender, EventArgs e)
+        {
+            //get the current count of x from the label
+            x = Convert.ToInt32(lblX.Text);
+            x++;
+            lblX.Text = x.ToString();
+
+            //copy value from textbox to the label
+            lblFirstname.Text = txtFirstname.Text;
+
+            //also copy the value to the literal
+            ltlFirstname.Text = txtFirstname.Text;
+
+            //remove th text from the textbox
+            txtFirstname.Text = "";
+        }
+
+        protected void btnSelections_Click(object sender, EventArgs e)
+        {
+            //show the selected country in the literal control
+            ltlFirstname.Text = ddlCountry.SelectedValue + " " + ddlCountry.SelectedItem.Text;
+
+            //clear the toppings list
+            ltlToppings.Text = "";
+
+            //loop through the toppings and print out the selected topping list
+            foreach (ListItem item in cblToppings.Items)
+            {
+                if(item.Selected)
+                    ltlToppings.Text += item.Text + " ";
+            }
+        }
+    }
+}
